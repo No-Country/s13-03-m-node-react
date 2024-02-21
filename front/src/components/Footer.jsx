c8f06584e8d4e8740b6a609e5d11eaedf9b63496
pinoen <pinoen@yahoo.com.ar>
2024-02-21 13:37:38 -0300

    add margin top

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,15 +1,15 @@
 const Footer = () => {
     return (
-        <footer className="flex justify-between pt-4 pb-16 px-6 bg-[#1400ff1a]">        
-        <ul className="font-medium text-xs	">
-        <li>Contacto</li>
-        <li>Teléfono</li> 
-        <li>E-mail</li>
-        </ul>
-        <p className="flex items-end text-[10px] font-medium">Todos los derechos reservados</p>
-        <div className="bg-[#1400ff1a] rounded-[100%] px-2 py-6">Circulo</div>
+        <footer className="flex justify-between pt-4 pb-16 px-6 bg-[#1400ff1a] mt-4">
+            <ul className="font-medium text-xs	">
+                <li>Contacto</li>
+                <li>Teléfono</li>
+                <li>E-mail</li>
+            </ul>
+            <p className="flex items-end text-[10px] font-medium">Todos los derechos reservados</p>
+            <div className="bg-[#1400ff1a] rounded-[100%] px-2 py-6">Circulo</div>
         </footer>
     )
-  }
-  
-  export default Footer
+}
+
+export default Footer
