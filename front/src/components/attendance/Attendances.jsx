39107b9f719657688f9495fa14ade347a8c57627
pinoen <pinoen@yahoo.com.ar>
2024-02-22 15:26:48 -0300

    provide basic structure

--- front/src/components/attendance/Attendances.jsx
+++ front/src/components/attendance/Attendances.jsx
@@ -1,7 +1,76 @@
+import { Checkbox, Radio, RadioGroup } from "@nextui-org/react"
 
 const Attendances = () => {
   return (
-    <div className="text-3xl h-screen flex justify-center items-center">Attendances</div>
+    <div className="text-3xl h-screen w-full flex-col justify-center items-center">
+      <h1 className="text-2xl font-semibold text-center mt-10">Asistencias</h1>
+      <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
+        <div className="p-4">
+          <small>20/02/2024</small>
+        </div>
+        <div className="p-4">
+          <small>Febrero</small>
+        </div>
+        <Checkbox size="md" />
+      </div>
+
+      <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
+        <div className="p-4">
+          <small>21/02/2024</small>
+        </div>
+        <div className="p-4">
+          <small>Febrero</small>
+        </div>
+        <Checkbox size="md" />
+      </div>
+
+      <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
+        <div className="p-4">
+          <small>22/02/2024</small>
+        </div>
+        <div className="p-4">
+          <small>Febrero</small>
+        </div>
+        <Checkbox size="md" />
+      </div>
+
+      <h1 className="text-2xl font-semibold text-center mt-10">Historial</h1>
+      <RadioGroup orientation="horizontal" className="flex justify-center items-center  gap-1 mt-4 ">
+        <Radio size="md" value="mes">Mes pasada</Radio>
+        <Radio size="md" value="week">Semana pasada</Radio>
+        <Radio size="md" value="all">Todo</Radio>
+      </RadioGroup>
+
+      <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
+        <div className="p-4">
+          <small>14/02/2024</small>
+        </div>
+        <div className="p-4">
+          <small>Febrero</small>
+        </div>
+        <Checkbox size="lg" />
+      </div>
+
+      <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
+        <div className="p-4">
+          <small>15/02/2024</small>
+        </div>
+        <div className="p-4">
+          <small>Febrero</small>
+        </div>
+        <Checkbox size="md" />
+      </div>
+
+      <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
+        <div className="p-4">
+          <small>16/02/2024</small>
+        </div>
+        <div className="p-4">
+          <small>Febrero</small>
+        </div>
+        <Checkbox size="md" />
+      </div>
+    </div>
   )
 }
 
