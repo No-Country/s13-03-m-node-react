f22a04f6e0e6b372852ee9cc5cf43237221f543d
pinoen <pinoen@yahoo.com.ar>
2024-02-22 17:43:41 -0300

    remove div with a date

--- front/src/components/attendance/Attendances.jsx
+++ front/src/components/attendance/Attendances.jsx
@@ -43,16 +43,6 @@ const Attendances = () => {
 
       <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
         <div className="p-4">
-          <small>14/02/2024</small>
-        </div>
-        <div className="p-4">
-          <small>Febrero</small>
-        </div>
-        <Checkbox size="lg" />
-      </div>
-
-      <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
-        <div className="p-4">
           <small>15/02/2024</small>
         </div>
         <div className="p-4">
