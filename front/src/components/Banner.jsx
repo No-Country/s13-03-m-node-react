620008d6850386c76cb29d7b28f667ff2d83a094
PaulaVelezz <velezpaula.a@gmail.com>
2024-02-22 00:18:17 -0300

    pagina login y banner

--- front/src/components/Banner.jsx
+++ front/src/components/Banner.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { FaCheck } from 'react-icons/fa';
+
+
+const DateComponent = ({ date }) => {
+  return (
+    <div className='text-center'>
+      <p className='text-3xl font-medium'>{date}</p>
+    </div>
+  );
+};
+
+const CheckboxItem = ({ text }) => {
+  return (
+    <p className='flex justify-left items-center gap-2 text-base font-medium'>
+      <span><FaCheck /></span>
+      {text}
+    </p>
+  );
+};
+
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <section className='flex justify-center items-center gap-4 h-40 border-1 rounded-md border-gray-800 p-4 m-4 mb-16 max-w-5xl'>
+      <DateComponent date='21 Feb' />
+     
+      <div className='w-4/5 h-full rounded-md bg-no-repeat bg-cover' style={{backgroundImage: "url('/images/banner-img.webp')"}}>
+        <div className='rounded-md backdrop-blur-sm bg-white/50 m-2 p-2'>
+          <CheckboxItem text='Desinfección primaria' />
+          <CheckboxItem text='Inscripción jardín' />
+          <CheckboxItem text='Corte de agua' />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Banner;
--- front/src/pages/Register.jsx
+++ front/src/pages/Register.jsx
@@ -8,9 +8,9 @@ const Register = () => {
 
         <div className="flex items-center justify-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
           <div>
-            <h1 className="text-2xl font-semibold">Crea tu cuenta</h1>
+            <h1 className="text-2xl font-semibold">¡Te damos la bienvenida a Educlass!</h1>
             <p className="mt-4">
-              Regístrate ahora y desbloquea todos los beneficios. 
+              Completa el siguiente formulario para crear tu cuenta. 
             </p>
 
             <form action="" className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
@@ -26,9 +26,9 @@ const Register = () => {
               </div>
 
               <div>
-                <label className="block mb-2 text-sm">Código de seguridad</label>
+                <label className="block mb-2 text-sm">Código del alumno</label>
                 <input
-                  placeholder="Ingresa tu Código de seguridad"
+                  placeholder="Ingresa tu Código del Alumno"
                   type="text"
                   required
                   className="w-full px-4 py-2 border rounded-md focus:border-violet-500 focus:ring-violet-400 focus:outline-none focus:ring focus:ring-opacity-40"
