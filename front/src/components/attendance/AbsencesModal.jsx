d2c35ffdc8c8f00f1a2736d01986720b754f33ef
pinoen <pinoen@yahoo.com.ar>
2024-02-23 14:30:55 -0300

    change upload icon size

--- front/src/components/attendance/AbsencesModal.jsx
+++ front/src/components/attendance/AbsencesModal.jsx
@@ -3,16 +3,16 @@ import uploadIcon from '../../assets/icons/upload.png'
 
 const AbsencesModal = ({ isOpen, onOpenChange }) => {
   return (
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange} radius="md" size="sm" backdrop="blur" placement="center" disableAnimation={true}>
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} radius="md" size="xs" backdrop="blur" placement="center" disableAnimation={true}>
       <ModalContent>
         {() => (
           <>
             <ModalHeader className="flex flex-col gap-1 text-center">Justificaciones de ausencias</ModalHeader>
             <ModalBody>
-              <p>Desde mi computadora</p>
               <label htmlFor="justification" className="cursor-pointer relative inline-block">
-                <div className="w-[100px] h-[100px] bg-slate-200 rounded-large hover:opacity-70">
-                  <img src={uploadIcon} alt="upload icon" />
+                Desde mi computadora
+                <div className="w-[100px] h-[100px] bg-slate-200 rounded-large hover:opacity-70 mt-2 flex justify-center items-center ml-16">
+                  <img src={uploadIcon} alt="upload icon" className="w-[50px] h-[50px]" />
                 </div>
               </label>
               <Input id="justification" type="file" accept="image/*" className="hidden" />
