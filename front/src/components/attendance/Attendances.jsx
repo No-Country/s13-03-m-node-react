0b2c4081468aa5075995aab6018b33d92510e6e0
pinoen <pinoen@yahoo.com.ar>
2024-02-23 16:26:09 -0300

    redesign

--- front/src/components/attendance/Attendances.jsx
+++ front/src/components/attendance/Attendances.jsx
@@ -1,4 +1,3 @@
-import { Checkbox, Radio, RadioGroup } from "@nextui-org/react"
 
 const Attendances = () => {
   return (
@@ -6,32 +5,22 @@ const Attendances = () => {
       <h1 className="text-2xl font-semibold text-center mt-10">Asistencias</h1>
       <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
         <div className="p-4">
-          <small>20/02/2024</small>
+          <small>Febrero 2024</small>
         </div>
         <div className="p-4">
-          <small>Febrero</small>
+          <small>10 asistencias</small>
         </div>
-        <Checkbox size="md" />
+        {/* <Checkbox size="md" /> */}
       </div>
 
       <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
         <div className="p-4">
-          <small>21/02/2024</small>
+          <small>Acumulado 2024</small>
         </div>
         <div className="p-4">
-          <small>Febrero</small>
+          <small>20 asistencias</small>
         </div>
-        <Checkbox size="md" />
-      </div>
-
-      <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
-        <div className="p-4">
-          <small>22/02/2024</small>
-        </div>
-        <div className="p-4">
-          <small>Febrero</small>
-        </div>
-        <Checkbox size="md" />
+        {/* <Checkbox size="md" /> */}
       </div>
 
       <h1 className="text-2xl font-semibold text-center mt-10">Historial</h1>
