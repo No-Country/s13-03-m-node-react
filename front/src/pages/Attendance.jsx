d2b6e61ef21ceb1f2682239d54cf34b58ebbd92f
pinoen <pinoen@yahoo.com.ar>
2024-02-23 14:52:48 -0300

    change grid by flex and add dummy graph for metrics section

--- front/src/pages/Attendance.jsx
+++ front/src/pages/Attendance.jsx
@@ -1,14 +1,15 @@
-import { Card, CardHeader, Image } from "@nextui-org/react";
+import { Card, CardHeader, Image, Radio, RadioGroup } from "@nextui-org/react";
 import asistenciaPic from '../assets/images/asistencias.jpg';
 import ausenciaPic from '../assets/images/ausencias.jpg';
 import retirosPic from '../assets/images/retiros.jpg';
 import { useNavigate } from "react-router-dom";
+import graph from '../assets/icons/graph.png';
 
 const Attendance = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
+    <div className="max-w-[900px] gap-2 flex flex-col px-8">
       <Card className="col-span-12 sm:col-span-4 h-[300px] cursor-pointer">
         <CardHeader className="absolute z-10 flex-col !items-start shadow-sm bg-gradient-to-tr from-[#8f6c50f1] to-[#e42e161a]">
           <p className="text-tiny text-white/60 uppercase font-bold">EduClass</p>
@@ -48,6 +49,16 @@ const Attendance = () => {
           onClick={() => navigate("/asistencias/retiros")}
         />
       </Card>
+
+      <h1 className="text-2xl font-semibold text-center mt-10">Métricas</h1>
+      <RadioGroup orientation="horizontal" className="flex justify-center items-center  gap-4 mt-4 ">
+        <Radio size="md" value="mes">Asistencias</Radio>
+        <Radio size="md" value="week">Ausencias</Radio>
+      </RadioGroup>
+
+      <div>
+        <img src={graph} alt="graph" />
+      </div>
     </div>
   );
 }
