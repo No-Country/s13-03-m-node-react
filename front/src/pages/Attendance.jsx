014864552f22acf5365779910ca1afc97ba43421
pinoen <pinoen@yahoo.com.ar>
2024-02-24 14:00:15 -0300

    replace card by mapping through attendance card component

--- front/src/pages/Attendance.jsx
+++ front/src/pages/Attendance.jsx
@@ -1,54 +1,44 @@
-import { Card, CardHeader, Image, Radio, RadioGroup } from "@nextui-org/react";
+import { Radio, RadioGroup } from "@nextui-org/react";
 import asistenciaPic from '../assets/images/asistencias.jpg';
 import ausenciaPic from '../assets/images/ausencias.jpg';
 import retirosPic from '../assets/images/retiros.jpg';
 import { useNavigate } from "react-router-dom";
 import graph from '../assets/icons/graph.png';
+import AttendanceCard from "../components/attendance/AttendanceCard";
+
+const links = [
+  {
+    name: "Asistencias",
+    link: "/asistencias/asistencias",
+    image: asistenciaPic
+  },
+  {
+    name: "Ausencias",
+    link: "/asistencias/ausencias",
+    image: ausenciaPic
+  },
+  {
+    name: "Retiros",
+    link: "/asistencias/retiros",
+    image: retirosPic
+  },
+];
 
 const Attendance = () => {
   const navigate = useNavigate();
 
   return (
     <div className="max-w-[900px] gap-2 flex flex-col px-8">
-      <Card className="col-span-12 sm:col-span-4 h-[300px] cursor-pointer">
-        <CardHeader className="absolute z-10 flex-col !items-start shadow-sm bg-gradient-to-tr from-[#8f6c50f1] to-[#e42e161a]">
-          <p className="text-tiny text-white/60 uppercase font-bold">EduClass</p>
-          <h4 className="text-white font-medium text-large">Asistencias</h4>
-        </CardHeader>
-        <Image
-          removeWrapper
-          alt="asistencias"
-          className="z-0 w-full h-full object-cover"
-          src={asistenciaPic}
-          onClick={() => navigate("/asistencias/asistencias")}
-        />
-      </Card>
-      <Card className="col-span-12 sm:col-span-4 h-[300px] cursor-pointer">
-        <CardHeader className="absolute z-10 flex-col !items-start shadow-sm bg-gradient-to-tr from-[#8f6c50f1] to-[#e42e161a]">
-          <p className="text-tiny text-white/60 uppercase font-bold">EduClass</p>
-          <h4 className="text-white font-medium text-large">Ausencias</h4>
-        </CardHeader>
-        <Image
-          removeWrapper
-          alt="ausencias"
-          className="z-0 w-full h-full object-cover"
-          src={ausenciaPic}
-          onClick={() => navigate("/asistencias/ausencias")}
-        />
-      </Card>
-      <Card className="col-span-12 sm:col-span-4 h-[300px] cursor-pointer">
-        <CardHeader className="absolute z-10 flex-col !items-start shadow-sm bg-gradient-to-tr from-[#8f6c50f1] to-[#e42e161a]">
-          <p className="text-tiny text-white/60 uppercase font-bold">EduClass</p>
-          <h4 className="text-white font-medium text-large">Retiros</h4>
-        </CardHeader>
-        <Image
-          removeWrapper
-          alt="retiros"
-          className="z-0 w-full h-full object-cover"
-          src={retirosPic}
-          onClick={() => navigate("/asistencias/retiros")}
-        />
-      </Card>
+      {
+        links.map((link) => (
+          <AttendanceCard
+            key={link.name}
+            name={link.name}
+            image={link.image}
+            handleNavigate={() => navigate(link.link)}
+          />
+        ))
+      }
 
       <h1 className="text-2xl font-semibold text-center mt-10">Métricas</h1>
       <RadioGroup orientation="horizontal" className="flex justify-center items-center  gap-4 mt-4 ">
