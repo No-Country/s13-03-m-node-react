aaa4c9fc8cc23b4ed9b882d85864809fe6b598e4
pinoen <pinoen@yahoo.com.ar>
2024-02-25 17:37:15 -0300

    render data using use location hook and filter months to see history

--- front/src/components/attendance/Attendances.jsx
+++ front/src/components/attendance/Attendances.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react"
+import { useLocation } from "react-router-dom"
 
 const Attendances = () => {
+  const { state } = useLocation()
+  const { enero, febrero, marzo, abril, mayo, junio, julio, agosto, septiembre, octubre, noviembre, diciembre } = state
+  const acumulado_anual = enero + febrero + marzo + abril + mayo + junio + julio + agosto + septiembre + octubre + noviembre + diciembre
+  const [month, setMonth] = useState("enero")
+
   return (
     <div className="text-3xl h-screen w-full flex-col justify-center items-center">
       <h1 className="text-2xl font-semibold text-center mt-10">Asistencias</h1>
@@ -8,9 +15,8 @@ const Attendances = () => {
           <small>Febrero 2024</small>
         </div>
         <div className="p-4">
-          <small>10 asistencias</small>
+          <small>{state.febrero} asistencias</small>
         </div>
-        {/* <Checkbox size="md" /> */}
       </div>
 
       <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
@@ -18,28 +24,39 @@ const Attendances = () => {
           <small>Acumulado 2024</small>
         </div>
         <div className="p-4">
-          <small>20 asistencias</small>
+          <small>{acumulado_anual} asistencias</small>
         </div>
-        {/* <Checkbox size="md" /> */}
       </div>
 
       <h1 className="text-2xl font-semibold text-center mt-10">Historial</h1>
 
       <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
-        <select>
-          <option value="marzo">Marzo</option>
-          <option value="abril">Abril</option>
-          <option value="mayo">Mayo</option>
-          <option value="junio">Junio</option>
-          <option value="julio">Julio</option>
-          <option value="agosto">Agosto</option>
-          <option value="septiembre">Septiembre</option>
-          <option value="octubre">Octubre</option>
-          <option value="noviembre">Noviembre</option>
-          <option value="diciembre">Diciembre</option>
+        <select className="w-full" onChange={(e) => setMonth(e.target.value)} value={month}>
+          <option value="enero">1 - Enero</option>
+          <option value="febrero">2 - Febrero</option>
+          <option value="marzo">3 - Marzo</option>
+          <option value="abril">4 - Abril</option>
+          <option value="mayo">5 - Mayo</option>
+          <option value="junio">6 - Junio</option>
+          <option value="julio">7 - Julio</option>
+          <option value="agosto">8 - Agosto</option>
+          <option value="septiembre">9 - Septiembre</option>
+          <option value="octubre">10 - Octubre</option>
+          <option value="noviembre">11 - Noviembre</option>
+          <option value="diciembre">12 - Diciembre</option>
         </select>
       </div>
+
+      <div className="border-1 rounded-md border-gray-800 p-2 m-2 flex justify-between">
+        <div className="p-4">
+          <small>{month.toUpperCase()} 2024</small>
+        </div>
+        <div className="p-4">
+          <small>{state[month]} asistencias</small>
+        </div>
+      </div>
     </div>
+
   )
 }
 
