4ae5f9e77d098a29dd19412d4a6c38ada9635a61
pinoen <pinoen@yahoo.com.ar>
2024-02-26 14:57:02 -0300

    import links array from utils so to map over it

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,24 +1,12 @@
-import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, Link, NavbarItem, Button } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, Link, NavbarItem, Button, link } from "@nextui-org/react";
 import { useState } from "react";
 import Logo from "./Logo";
 import ProfilePic from "./ProfilePic";
+import { links } from "../utils/links";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-  const menuItems = [
-    "Notificaciones",
-    "Actividades",
-    "Calendario",
-    "Chat",
-    "Asistencias",
-    "Calificaciones",
-    "Ayuda",
-    "Perfil",
-    "Login",
-    "Sign-Up",
-  ];
 
   return (
     <Navbar
@@ -50,15 +38,16 @@ const Header = () => {
       </NavbarContent>}
 
       <NavbarMenu className="bg-gray-100 overflow-hidden w-[250px] rounded">
-        {menuItems.map((item, index) => (
+        {links.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
+
             <Link
-              className="w-full hover:bg-gray-200 hover:font-semibold"
+              className="w-full hover:bg-gray-200 hover:font-semibold gap-2"
               color="foreground"
-              href={`/${item.toLowerCase()}`}
+              href={`/${item.path}`}
               size="lg"
             >
-              {item}
+              {item.icon} {item.text}
             </Link>
           </NavbarMenuItem>
         ))}
