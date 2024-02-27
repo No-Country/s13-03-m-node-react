018cdd1054506f6132b39a552d5aad2f7f63c622
pinoen <pinoen@yahoo.com.ar>
2024-02-27 17:54:03 -0300

    create attendance block component

---
+++ front/src/components/attendance/AttendanceBlock.jsx
@@ -0,0 +1,23 @@
+import { Button } from "@nextui-org/react"
+
+const AttendanceBlock = ({ title, total, handleClick }) => {
+
+  return (
+    <>
+      <h1 className="text-2xl font-semibold text-left mt-8 text-[#280058]">{title}</h1>
+
+      <div className="border-1 rounded-md border-orange-500 shadow-lg shadow-orange-500 mt-4 flex justify-between">
+        <div className="p-4 text-2xl">
+          <small>Acumulado 2024</small>
+        </div>
+        <div className="p-4 text-2xl">
+          <small className="text-[#280058]">{total} {title.toLowerCase()}</small>
+        </div>
+      </div>
+      {title !== 'Asistencias' && <Button onClick={handleClick} variant="ghost" color="secondary" size="lg" className="mt-3 self-end font-semibold">{title === 'Ausencias' ? "Justificar" : "Ir al formulario"}</Button>
+      }
+    </>
+  )
+}
+
+export default AttendanceBlock
