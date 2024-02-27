719cafb2443fd4e7dfdcd463551ac124ceaf342a
pinoen <pinoen@yahoo.com.ar>
2024-02-27 15:50:12 -0300

    import month selection component and refactor code

--- front/src/components/attendance/Attendances.jsx
+++ front/src/components/attendance/Attendances.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useLocation } from "react-router-dom"
+import MonthSelection from "./MonthSelection"
 
 const Attendances = () => {
   const { state } = useLocation()
@@ -30,25 +31,10 @@ const Attendances = () => {
 
       <h1 className="text-2xl font-semibold text-center mt-10">Historial</h1>
 
-      <div className="border-1 rounded-md border-orange-500 shadow-lg shadow-orange-500 p-1 m-2 flex justify-between bg-white">
-        <select className="w-full text-xl bg-white" onChange={(e) => setMonth(e.target.value)} value={month}>
-          <option value="enero">Enero</option>
-          <option value="febrero">Febrero</option>
-          <option value="marzo">Marzo</option>
-          <option value="abril">Abril</option>
-          <option value="mayo">Mayo</option>
-          <option value="junio">Junio</option>
-          <option value="julio">Julio</option>
-          <option value="agosto">Agosto</option>
-          <option value="septiembre">Septiembre</option>
-          <option value="octubre">Octubre</option>
-          <option value="noviembre">Noviembre</option>
-          <option value="diciembre">Diciembre</option>
-        </select>
-      </div>
+      <MonthSelection handleChange={(e) => setMonth(e.target.value)} month={month} />
 
-      <div className="border-1 rounded-md border-orange-500 shadow-lg shadow-orange-500 p-1 m-2 flex justify-between mt-6">
-        <div className="p-4 text-2xl">
+      <div className="border-1 rounded-md border-orange-500 shadow-lg shadow-orange-500 p-1 m-2 flex justify-between items-center mt-6">
+        <div className="p-4 text-2xl w-10">
           <small>{month.toUpperCase()} 2024</small>
         </div>
         <div className="p-4 text-2xl">
