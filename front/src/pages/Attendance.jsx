bfe9ab0dfe048f38a5c67aad7bcf8ac57b775660
pinoen <pinoen@yahoo.com.ar>
2024-02-27 16:14:33 -0300

    create data.js and import it in attendance component

--- front/src/pages/Attendance.jsx
+++ front/src/pages/Attendance.jsx
@@ -1,72 +1,7 @@
-import asistenciaPic from '../assets/images/asistencias.jpg';
-import ausenciaPic from '../assets/images/ausencias.jpg';
-import retirosPic from '../assets/images/retiros.jpg';
 import { useNavigate } from "react-router-dom";
 import AttendanceCard from "../components/attendance/AttendanceCard";
 import AttendanceChart from '../components/attendance/AttendanceChart';
-
-const data = [
-  {
-    name: "Asistencias",
-    link: "/asistencias/asistencias",
-    image: asistenciaPic,
-    acumulado_mensual: 15,
-    acumulado_anual: 35,
-    enero: 18,
-    febrero: 17,
-    marzo: 0,
-    abril: 0,
-    mayo: 0,
-    junio: 0,
-    julio: 0,
-    agosto: 0,
-    septiembre: 0,
-    octubre: 0,
-    noviembre: 0,
-    diciembre: 0,
-    total_mensual: 20,
-    total_anual: 200
-  },
-  {
-    name: "Ausencias",
-    link: "/asistencias/ausencias",
-    image: ausenciaPic,
-    acumulado_mensual: 2,
-    acumulado_anual: 3,
-    total_anual: 10,
-    enero: 1,
-    febrero: 2,
-    marzo: 0,
-    abril: 0,
-    mayo: 0,
-    junio: 0,
-    julio: 0,
-    agosto: 0,
-    septiembre: 0,
-    octubre: 0,
-    noviembre: 0,
-    diciembre: 0,
-  },
-  {
-    name: "Retiros",
-    link: "/asistencias/retiros",
-    image: retirosPic,
-    acumulado_mensual: 3,
-    acumulado_anual: 4,
-    enero: 1,
-    febrero: 3,
-    marzo: 0,
-    abril: 0,
-    mayo: 0,
-    junio: 0,
-    julio: 0,
-    agosto: 0,
-    septiembre: 0,
-    octubre: 0,
-    noviembre: 0,
-    diciembre: 0,
-  },
-];
+import { data } from '../utils/data.js';
 
 const Attendance = () => {
   const navigate = useNavigate();
