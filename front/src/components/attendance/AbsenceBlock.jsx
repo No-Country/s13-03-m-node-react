e204ae5452163f7103eddcf27e776ed7ae0b7356
pinoen <pinoen@yahoo.com.ar>
2024-02-27 23:12:38 -0300

    create absence block component

---
+++ front/src/components/attendance/AbsenceBlock.jsx
@@ -0,0 +1,22 @@
+import { useDisclosure } from "@nextui-org/react";
+import AbsencesModal from "./AbsencesModal";
+
+const AbsenceBlock = ({ date }) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <div className="border-1 rounded-md border-orange-500 shadow-lg shadow-orange-500 mt-4 flex justify-between m-4">
+        <div className="p-4 text-2xl">
+          <small>{date}</small>
+        </div>
+        <div className="p-4 text-2xl">
+          <small className="text-[#3FA3EB] font-medium cursor-pointer" onClick={onOpen}>Justificar</small>
+          <AbsencesModal isOpen={isOpen} onOpenChange={onOpenChange} />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AbsenceBlock
