d5d64d9134159ad0eac330794424470471a90736
pinoen <pinoen@yahoo.com.ar>
2024-02-28 18:09:41 -0300

    add handleSubmit fn

--- front/src/components/attendance/AbsencesModal.jsx
+++ front/src/components/attendance/AbsencesModal.jsx
@@ -3,17 +3,34 @@ import uploadIcon from '../../assets/icons/upload.png'
 import { useState } from "react"
 import SkeletonImage from "./SkeletonImage"
 import Swal from "sweetalert2"
+import axios from "axios"
 
 const AbsencesModal = ({ isOpen, onOpenChange }) => {
   const [justification, setJustification] = useState(null)
 
-  const handleSend = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    formData.append('justification', justification)
     onOpenChange(false)
-    Swal.fire({
-      icon: 'success',
-      title: 'Justificación enviada',
-      text: 'Se ha enviado tu justificación',
-    })
+
+    try {
+      await axios.post('https://educlass-2024.onrender.com/api/attendance', formData)
+      console.log(justification)
+      console.log(formData)
+      Swal.fire({
+        icon: 'success',
+        title: 'Justificación enviada',
+        text: 'Se ha enviado tu justificación',
+      })
+    } catch (error) {
+      console.log(error)
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Hubo un error al enviar tu justificación',
+      })
+    }
   }
 
   return (
@@ -21,23 +38,25 @@ const AbsencesModal = ({ isOpen, onOpenChange }) => {
       <ModalContent>
         {() => (
           <>
-            <ModalHeader className="flex flex-col gap-1 text-left">Justificaciones de ausencias</ModalHeader>
-            <ModalBody>
-              <label htmlFor="justification" className="cursor-pointer relative inline-block">
-                Subir documento
-                <div className="w-[270px] h-[200px] bg-slate-200 rounded-large hover:opacity-70 mt-2 flex justify-center items-center">
-                  <img src={uploadIcon} alt="upload icon" className="w-[50px] h-[50px]" />
-                </div>
-              </label>
-              <Input id="justification" type="file" accept="image/*" className="hidden"
-                onChange={(e) => setJustification(URL.createObjectURL(e.target.files[0]))} />
+            <form method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
+              <ModalHeader className="flex flex-col gap-1 text-left">Justificaciones de ausencias</ModalHeader>
+              <ModalBody>
+                <label htmlFor="justification" className="cursor-pointer relative inline-block">
+                  Subir documento
+                  <div className="w-[270px] h-[200px] bg-slate-200 rounded-large hover:opacity-70 mt-2 flex justify-center items-center">
+                    <img src={uploadIcon} alt="upload icon" className="w-[50px] h-[50px]" />
+                  </div>
+                </label>
+                <Input id="justification" name="justification" type="file" accept="image/*" className="hidden"
+                  onChange={(e) => setJustification(URL.createObjectURL(e.target.files[0]))} />
 
-              {!justification && <SkeletonImage />}
+                {!justification && <SkeletonImage />}
 
-              {justification && <img src={justification} alt="justification" className="w-[270px] mt-2" id="justification" />}
+                {justification && <img src={justification} alt="justification" className="w-[270px] mt-2" id="justification" />}
 
-              <Button className="w-full mt-3 shadow-inner bg-[#7222D3]" color="secondary" onClick={handleSend}>Enviar</Button>
-            </ModalBody>
+                <Button className="w-full mt-3 shadow-inner bg-[#7222D3]" color="secondary" type="submit">Enviar</Button>
+              </ModalBody>
+            </form>
           </>
         )}
 
