4aa16140f58fae6f6084a390420ee4a1dc01991d
pinoen <pinoen@yahoo.com.ar>
2024-03-01 11:12:29 -0300

    create attendance loader

--- front/src/pages/Attendance.jsx
+++ front/src/pages/Attendance.jsx
@@ -1,23 +1,35 @@
-import { useNavigate } from "react-router-dom";
+/* eslint-disable react-refresh/only-export-components */
+import { redirect, useLoaderData, useNavigate } from "react-router-dom";
 import AttendanceChart from '../components/attendance/AttendanceChart';
-import { data } from '../utils/data.js';
+import { data as mockData } from '../utils/data.js';
 import AttendanceBlock from "../components/attendance/AttendanceBlock.jsx";
 import MonthSelection from "../components/attendance/MonthSelection.jsx";
 import { useState } from "react";
+import axios from "axios";
 
+export const loader = async () => {
+  try {
+    const { data } = await axios.get('https://educlass-2024.onrender.com/api/attendance');
+    return data
+  } catch (error) {
+    return redirect('/asistencias')
+  }
+}
 const Attendance = () => {
+  const { data } = useLoaderData();
+  console.log(data.document)
   const [month, setMonth] = useState("enero")
   const navigate = useNavigate();
 
   return (
     <div className="max-w-[900px] gap-2 flex flex-col px-4">
-      {data.map((link) => (
+      {mockData.map((link) => (
         <AttendanceBlock key={link.name} title={link.name} total={link.acumulado_mensual} handleClick={() => navigate(link.link, { state: link })} />
       ))}
 
-      <MonthSelection handleChange={(e) => setMonth(e.target.value)} month={month} data={data} />
+      <MonthSelection handleChange={(e) => setMonth(e.target.value)} month={month} data={mockData} />
 
-      <AttendanceChart data={data} />
+      <AttendanceChart data={mockData} />
     </div>
   );
 }
