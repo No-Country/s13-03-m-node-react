b64904728369c6a51ae6d5c03b83525215464f7f
pinoen <pinoen@yahoo.com.ar>
2024-03-01 11:14:10 -0300

    change fn to send image file

--- front/src/components/attendance/AbsencesModal.jsx
+++ front/src/components/attendance/AbsencesModal.jsx
@@ -1,3 +1,75 @@
+// import { Modal, ModalContent, ModalHeader, ModalBody, Button } from "@nextui-org/react"
+// import uploadIcon from '../../assets/icons/upload.png'
+// import { useState } from "react"
+// import SkeletonImage from "./SkeletonImage"
+// import Swal from "sweetalert2"
+// import axios from "axios"
+
+// const AbsencesModal = ({ isOpen, onOpenChange }) => {
+//   const [justification, setJustification] = useState(null)
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault()
+//     const formData = new FormData(e.target)
+//     formData.append('justification', justification)
+//     onOpenChange(false)
+//     console.log(justification)
+//     console.log(formData)
+
+//     try {
+//       await axios.put('https://educlass-2024.onrender.com/api/attendance/update/?_id=65de921dc4b8e7383f8d3acd', formData)
+//       console.log(justification)
+//       console.log(formData)
+//       Swal.fire({
+//         icon: 'success',
+//         title: 'Justificación enviada',
+//         text: 'Se ha enviado tu justificación',
+//       })
+//     } catch (error) {
+//       console.log(error)
+//       Swal.fire({
+//         icon: 'error',
+//         title: 'Oops...',
+//         text: 'Hubo un error al enviar tu justificación',
+//       })
+//     }
+//   }
+
+//   return (
+//     <Modal isOpen={isOpen} onOpenChange={onOpenChange} radius="md" size="xs" backdrop="blur" placement="center" disableAnimation={true}>
+//       <ModalContent>
+//         {() => (
+//           <>
+//             <form method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
+//               <ModalHeader className="flex flex-col gap-1 text-left">Justificaciones de ausencias</ModalHeader>
+//               <ModalBody>
+//                 <label htmlFor="justification" className="cursor-pointer relative inline-block">
+//                   Subir documento
+//                   <div className="w-[270px] h-[200px] bg-slate-200 rounded-large hover:opacity-70 mt-2 flex justify-center items-center">
+//                     <img src={uploadIcon} alt="upload icon" className="w-[50px] h-[50px]" />
+//                   </div>
+//                 </label>
+//                 <input id="justification" name="justification" type="file" accept="image/*" className="hidden"
+//                   onChange={(e) => setJustification(URL.createObjectURL(e.target.files[0]))} />
+
+//                 {!justification && <SkeletonImage />}
+
+//                 {justification && <img src={justification} alt="justification" className="w-[270px] mt-2" id="justification" />}
+
+//                 <Button className="w-full mt-3 shadow-inner bg-[#7222D3]" color="secondary" type="submit">Enviar</Button>
+//               </ModalBody>
+//             </form>
+//           </>
+//         )}
+
+//       </ModalContent>
+//     </Modal>
+//   )
+// }
+
+// export default AbsencesModal
+
+
 import { Modal, ModalContent, ModalHeader, ModalBody, Button } from "@nextui-org/react"
 import uploadIcon from '../../assets/icons/upload.png'
 import { useState } from "react"
@@ -6,9 +78,76 @@ import Swal from "sweetalert2"
 import axios from "axios"
 
 const AbsencesModal = ({ isOpen, onOpenChange }) => {
-  const [justification, setJustification] = useState(null)
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
 
-  const handleSubmit = async (e) => {
+  const onFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setImageUrl(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const onFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('image', selectedFile);
+
+    console.log(selectedFile)
+    console.log(imageUrl)
+    console.log(formData)
+
+    try {
+      await axios.put('https://educlass-2024.onrender.com/api/attendance/update/?_id=65de921dc4b8e7383f8d3acd', formData)
+      Swal.fire({
+        icon: 'success',
+        title: 'Justificación enviada',
+        text: 'Se ha enviado tu justificación',
+      })
+    } catch (error) {
+      console.log(error)
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Hubo un error al enviar tu justificación',
+      })
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} radius="md" size="xs" backdrop="blur" placement="center" disableAnimation={true}>
+      <ModalContent>
+        {() => (
+          <>
+            <form onSubmit={onFormSubmit}>
+              <ModalHeader className="flex flex-col gap-1 text-left">Justificaciones de ausencias</ModalHeader>
+              <ModalBody>
+                <label htmlFor="justification" className="cursor-pointer relative inline-block">
+                  Subir documento
+                  <div className="w-[270px] h-[200px] bg-slate-200 rounded-large hover:opacity-70 mt-2 flex justify-center items-center">
+                    <img src={uploadIcon} alt="upload icon" className="w-[50px] h-[50px]" />
+                  </div>
+                </label>
+                <input id="justification" type="file" accept="image/*" className="hidden"
+                  onChange={onFileChange} />
+
+                {!imageUrl && <SkeletonImage />}
+
+                {imageUrl && <img src={imageUrl} alt="justification" className="w-[270px] mt-2" />}
+
+                <Button className="w-full mt-3 shadow-inner bg-[#7222D3]" color="secondary" type="submit">Enviar</Button>
+              </ModalBody>
+            </form>
+          </>
+        )}
+
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default AbsencesModal
+
+/* const handleSubmit = async (e) => {
     e.preventDefault()
     const formData = new FormData(e.target)
     formData.append('justification', justification)
@@ -34,37 +173,4 @@ const AbsencesModal = ({ isOpen, onOpenChange }) => {
       })
     }
   }
-
-  return (
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange} radius="md" size="xs" backdrop="blur" placement="center" disableAnimation={true}>
-      <ModalContent>
-        {() => (
-          <>
-            <form method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
-              <ModalHeader className="flex flex-col gap-1 text-left">Justificaciones de ausencias</ModalHeader>
-              <ModalBody>
-                <label htmlFor="justification" className="cursor-pointer relative inline-block">
-                  Subir documento
-                  <div className="w-[270px] h-[200px] bg-slate-200 rounded-large hover:opacity-70 mt-2 flex justify-center items-center">
-                    <img src={uploadIcon} alt="upload icon" className="w-[50px] h-[50px]" />
-                  </div>
-                </label>
-                <input id="justification" name="justification" type="file" accept="image/*" className="hidden"
-                  onChange={(e) => setJustification(URL.createObjectURL(e.target.files[0]))} />
-
-                {!justification && <SkeletonImage />}
-
-                {justification && <img src={justification} alt="justification" className="w-[270px] mt-2" id="justification" />}
-
-                <Button className="w-full mt-3 shadow-inner bg-[#7222D3]" color="secondary" type="submit">Enviar</Button>
-              </ModalBody>
-            </form>
-          </>
-        )}
-
-      </ModalContent>
-    </Modal>
-  )
-}
-
-export default AbsencesModal
+*/
