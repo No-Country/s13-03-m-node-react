9f67ee495178f4e6a02ab8949f20bedc54b823e4
pinoen <pinoen@yahoo.com.ar>
2024-03-03 23:16:48 -0300

    create chartData array to pass as prop tp the chart component

--- front/src/pages/Attendance.jsx
+++ front/src/pages/Attendance.jsx
@@ -6,6 +6,8 @@ import AttendanceBlock from "../components/attendance/AttendanceBlock.jsx";
 import MonthSelection from "../components/attendance/MonthSelection.jsx";
 import { useState } from "react";
 import axios from "axios";
+import { countDaysInMonth, getCurrentMonth, getWorkingDaysSinceStartOfMonth, hasMonthPassed, getTotalAsistencias } from "../utils/months";
+
 
 export const loader = async () => {
   try {
@@ -20,15 +22,40 @@ const Attendance = () => {
   const [month, setMonth] = useState("enero")
   const navigate = useNavigate();
 
+  const ausencias = data.data.document.filter((item) => item.status === 'ausencia').map((item) => item.date)
+  const retiros = data.data.document.filter((item) => item.status === 'retiro').map((item) => item.date)
+  const asistencias = hasMonthPassed(month) ? data.total_month - countDaysInMonth(ausencias, month) : getCurrentMonth() === month ? getWorkingDaysSinceStartOfMonth() - countDaysInMonth(ausencias, month) : 0
+
+  const chartData = [
+    {
+      name: 'Asistencias',
+      total_anual: getTotalAsistencias(data.total_month),
+      acumulado_anual: getTotalAsistencias(data.total_month) - ausencias.length,
+      total_mensual: getWorkingDaysSinceStartOfMonth(),
+      acumulado_mensual: getWorkingDaysSinceStartOfMonth() - countDaysInMonth(ausencias, getCurrentMonth()),
+    },
+    {
+      name: 'Ausencias',
+      acumulado_anual: ausencias.length,
+      acumulado_mensual: countDaysInMonth(ausencias, getCurrentMonth()),
+      total_anual: 10,
+    },
+    {
+      name: 'Retiros',
+      acumulado_anual: retiros.length,
+      acumulado_mensual: countDaysInMonth(retiros, getCurrentMonth()),
+    }
+  ]
+
   return (
     <div className="max-w-[900px] gap-2 flex flex-col px-4">
       {mockData.map((link) => (
         <AttendanceBlock key={link.name} title={link.name} total={link.acumulado_mensual} handleClick={() => navigate(link.link, { state: link })} />
       ))}
 
-      <MonthSelection handleChange={(e) => setMonth(e.target.value)} month={month} data={data} />
+      <MonthSelection handleChange={(e) => setMonth(e.target.value)} month={month} asistencias={asistencias} ausencias={ausencias} retiros={retiros} />
 
-      <AttendanceChart mockData={mockData} />
+      <AttendanceChart chartData={chartData} />
     </div>
   );
 }
