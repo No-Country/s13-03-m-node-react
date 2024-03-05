d5e54c47f8ef3d0717432a4d1433138b0457ed67
PaulaVelezz <velezpaula.a@gmail.com>
2024-03-04 21:05:56 -0300

    integracion de api con banner

--- front/src/components/Banner.jsx
+++ front/src/components/Banner.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaCheck } from 'react-icons/fa';
+import axios from 'axios';
 
 
 const DateComponent = ({ date }) => {
@@ -21,15 +22,39 @@ const CheckboxItem = ({ text }) => {
 
 
 const Banner = () => {
+  const [notification, setNotification] = useState(null);
+
+  useEffect(() => {
+    const fetchNotification = async () => {
+      try {
+        const response = await axios.get('https://educlass-2024.onrender.com/api/notification');
+        const data = response.data.data;
+
+        console.log('Notification data:', data);
+
+        const sortedNotifications = data.document.sort((a, b) => new Date(b.fechaCreacion) - new Date(a.fechaCreacion));
+        const latestNotification = sortedNotifications[0];
+
+        setNotification(latestNotification);
+      } catch (error) {
+        console.error('Error fetching notification:', error);
+      }
+    };
+
+    fetchNotification();
+  }, []);
+
   return (
     <section className='flex justify-center items-center gap-4 h-40 border-1 rounded-md border-gray-800 p-4 m-4 mb-16 max-w-5xl'>
-      <DateComponent date='21 Feb' />
+      <DateComponent date={new Date().toLocaleDateString('es-ES', { day: 'numeric', month: 'short' })} />
      
       <div className='w-4/5 h-full rounded-md bg-no-repeat bg-cover' style={{backgroundImage: "url('src/assets/images/banner-img.webp')"}}>
         <div className='rounded-md backdrop-blur-sm bg-white/50 m-2 p-2'>
-          <CheckboxItem text='Desinfección primaria' />
-          <CheckboxItem text='Inscripción jardín' />
-          <CheckboxItem text='Corte de agua' />
+        {notification && (
+          <>
+          <CheckboxItem text={notification.titulo} />
+          </>
+        )}
         </div>
       </div>
     </section>
