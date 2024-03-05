c1a204278e70c6bcaf29abbf055f4e130f39292e
pinoen <pinoen@yahoo.com.ar>
2024-03-05 15:37:59 -0300

    adjust some styles

--- front/src/components/attendance/AttendanceBlock.jsx
+++ front/src/components/attendance/AttendanceBlock.jsx
@@ -6,15 +6,15 @@ const AttendanceBlock = ({ title, total, handleClick }) => {
     <>
       <h1 className="text-2xl font-semibold text-left mt-8 text-[#280058]">{title}</h1>
 
-      <div className="border-1 rounded-md border-orange-300 shadow-md shadow-orange-300 mt-4 flex justify-between">
-        <div className="p-4 text-2xl">
+      <div id="attendance-block" className="border-1 rounded-md border-orange-300 shadow-md shadow-orange-300 mt-4 flex justify-between bg-[#FFFEF9]">
+        <div className="p-4 text-sm text-[#280058]">
           <small>Acumulado 2024</small>
         </div>
-        <div className="p-4 text-2xl">
+        <div className="p-4 text-sm">
           <small className="text-[#280058]">{total} {title.toLowerCase()}</small>
         </div>
       </div>
-      {title !== 'Asistencias' && <Button onClick={handleClick} variant="ghost" color="secondary" size="lg" className="mt-3 self-end font-semibold">{title === 'Ausencias' ? "Justificar" : "Ir al formulario"}</Button>
+      {title !== 'Asistencias' && <Button onClick={handleClick} variant="light" color="secondary" size="lg" className="mt-3 self-end font-semibold">{title === 'Ausencias' ? "Justificar" : "Ir al formulario"}</Button>
       }
     </>
   )
