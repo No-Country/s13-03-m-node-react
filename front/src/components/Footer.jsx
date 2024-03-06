4ee7c8624d8ba71f18ece35de4d3e8d6cf3de410
Valentino <valentinoindorato@gmail.com>
2024-03-06 09:20:39 -0300

    Style component

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -3,38 +3,42 @@ import Logo from "./Logo";
 import { BiCopyright } from "react-icons/bi";
 import { FiPhone } from "react-icons/fi";
 import { MdOutlineEmail } from "react-icons/md";
-import { FaFacebook, FaInstagram  } from "react-icons/fa";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import {Avatar} from "@nextui-org/react";
 
 const Footer = () => {
   return (
     <footer className="flex justify-between relative  bottom-0  pt-4 pb-10 px-6 bg-[#FDFBFF] mt-4 border-t-2 border-[#28005880] text-[#280058]">
       <NavLink
         to="/"
-        className="flex flex-col items-center font-bold text-inherit "
+        className="flex flex-col items-center font-bold text-inherit mt-1 "
       >
-        <Logo />
-        <p className="font-bold text-inherit">EduClass</p>
+        <Logo footer={true} />
+        <p className="font-bold text-inherit text-[#7222D3]">EduClass</p>
       </NavLink>
-      <div className="  absolute  inset-x-24 bottom-0 pb-3 pl-2
- ">
+      <div className="  absolute  inset-x-24 bottom-0 pb-3 pl-2">
         <p className="flex items-center text-[10px] font-medium  gap-1">
           <span>
-            <BiCopyright />
+            <BiCopyright size={16} />
           </span>
           Todos los derechos reservados
         </p>
       </div>
-      <ul className=" text-xs	">
-      <li className="font-semibold	text-sm">Síguenos</li>
-        <ul className="flex gap-2">
-            <li><FaInstagram size={20} /></li>
-            <li><FaFacebook  size={20}/></li>
+      <ul className=" text-xs flex flex-col	gap-4">
+       
+        <ul >
+          <li className="font-semibold	text-sm mb-1">Síguenos</li>
+          <ul className="flex gap-2">
+          <li><FaInstagram size={20} /></li>
+          <li><FaFacebook size={20} /></li>
+          </ul>
         </ul>
-        <li className="font-semibold	text-sm">Contáctanos</li>
+       
         <ul>
-          <li className="flex items-center gap-1.5">
+        <li className="font-semibold	text-sm mb-1">Contáctanos</li>
+          <li className="flex items-center gap-1.5 mb-1">
             <span >
-              <FiPhone  size="1rem" />
+              <FiPhone size="1rem" />
             </span>
             (011) 4812-9843
           </li>
@@ -45,12 +49,12 @@ const Footer = () => {
             info@colegiosanpedro.com
           </li>
         </ul>
-        <li className="font-semibold	text-sm">Legal</li>
+      
         <ul>
+        <li className="font-semibold	text-sm mb-1">Legal</li>
           <li>Políticas de seguridad</li>
         </ul>
-        <Link to="/ayuda">
-        <li className="font-semibold	text-sm"> Ayuda</li></Link>
+
       </ul>
     </footer>
   );
--- front/src/pages/Grades.jsx
+++ front/src/pages/Grades.jsx
@@ -60,8 +60,8 @@ const Grades = () => {
     content: "text-small px-2 mx-2 my-3  ",
   };
   return (
-    <section className=" mt-8  text-[#280058] h-screen">
-      <h1 className="mx-4 mb-8 font-bold text-xl 	">
+    <section className=" mt-7  text-[#280058] h-screen">
+      <h1 className="mx-4 mb-10 font-bold text-xl 	">
         Sigue mi recorrido académico
       </h1>
       <Accordion showDivider={false} itemClasses={itemClasses}>
