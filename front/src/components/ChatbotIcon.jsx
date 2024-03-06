1aa8501ed827c89c5a745f884a81ccc22aa28069
pinoen <pinoen@yahoo.com.ar>
2024-03-06 13:43:56 -0300

    pau image import

--- front/src/components/ChatbotIcon.jsx
+++ front/src/components/ChatbotIcon.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 // import { HiOutlineChatAlt } from 'react-icons/hi';
+import bot from '../assets/images/Rectangle 695.png'
 
 const ChatbotIcon = ({ onClick }) => {
   return (
     <div className='fixed right-2 bottom-0 rounded-full cursor-pointer z-50 mb-80' onClick={onClick}>
       {/* <HiOutlineChatAlt onClick={onClick} /> */}
-      <img src="src/assets/images/Rectangle 695.png" alt="" className='w-16 ' />
+      <img src={bot} alt="" className='w-16 ' />
     </div>
   );
 }
