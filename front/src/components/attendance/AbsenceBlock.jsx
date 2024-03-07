a318e44caf803813bbeff60cc9843e68b929d6a7
pinoen <pinoen@yahoo.com.ar>
2024-03-06 22:19:08 -0300

    takes ausencia id which is intended to be justified an pass over as prop to the absences modal

--- front/src/components/attendance/AbsenceBlock.jsx
+++ front/src/components/attendance/AbsenceBlock.jsx
@@ -1,8 +1,12 @@
 import { useDisclosure } from "@nextui-org/react";
 import AbsencesModal from "./AbsencesModal";
+import { useLocation } from "react-router-dom"
+
 
 const AbsenceBlock = ({ date }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { state } = useLocation()
+  const id = state.ausenciasData.filter((item) => item.date === date).map((item) => item._id)[0]
 
   return (
     <>
@@ -12,7 +16,7 @@ const AbsenceBlock = ({ date }) => {
         </div>
         <div className="p-4 text-2xl">
           <small className="text-[#3FA3EB] font-medium cursor-pointer" onClick={onOpen}>Justificar</small>
-          <AbsencesModal isOpen={isOpen} onOpenChange={onOpenChange} />
+          <AbsencesModal isOpen={isOpen} onOpenChange={onOpenChange} id={id} />
         </div>
       </div>
     </>
